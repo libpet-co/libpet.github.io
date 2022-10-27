5b7473ab5e6cd2cd395b30d480eb8cf0389fe748
patrickcheung <patrickcheung@cmi.chinamobile.com>
2022-10-27 22:51:43 +0800

    27-10 22:50 new added

---
+++ js/team.js
@@ -0,0 +1,29 @@
+
+!function(t){
+    "use strict";
+    t("a.page-scroll").bind("click",function(a){
+        var o=t(this);t("html, body").stop().animate({
+            scrollTop:t(o.attr("href")).offset().top-50},
+        1250,
+        "easeInOutExpo"),
+        a.preventDefault()}),
+        t("body").scrollspy({
+            target:".navbar-fixed-top",
+            offset:100}),
+        t(".navbar-collapse ul li a").click(function(){
+            t(".navbar-toggle:visible").click()}),
+        t("#mainNav").affix({offset:{top:50}});
+
+        $(".carousel .owl-carousel").owlCarousel({
+            autoplay: true,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            items: 1,
+            smartSpeed: 10,
+            dots: false,
+            loop: true,
+            nav : false,
+        });
+
+}(jQuery);
+
