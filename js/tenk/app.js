23c41253ef9c20a74243ac394f46ee4f3e8fdddd
libpet-programming <libpet.programming@gmail.com>
2023-07-13 21:41:54 +0800

    tenk updated

--- js/tenk/app.js
+++ js/tenk/app.js
@@ -21,19 +21,25 @@
 // t.from(".img-2", {x: innerWidth * 1, duration: 3});
 // t.from(".img-3", {x: innerWidth * 1, duration: 3});
 // t.from(".img-4", {x: innerWidth * 1, duration: 3});
+// t.to(".img-4", {left: '10%', duration: 3})
 
 ScrollTrigger.create({
   trigger: '.app-container',
   start: 'top top',
-  end: '+=2000',
+  end: '+=8000',
   scrub: true,
-  markers: true,
+  // markers: true,
   pin: true,
   animation:
     gsap.timeline()
-    .to(".text", {scale: 0})
-    .from(".cover-img-1", {x: innerWidth * -2, duration: 3})
-    .from(".img-2", {x: innerWidth * 2, duration: 3})
-    .from(".img-3", {x: innerWidth * 2, duration: 3})
-    .from(".img-4", {x: innerWidth * 2, duration: 3})
+    .to(".caption", {scale: 0})
+    .from(".cover-img-1", {x: 1000 * -2, duration: 3})
+    .from(".img-2", {x: 1000 * 2, duration: 1})
+    .from(".img-3", {x: 1000 * 2, duration: 1})
+    .from(".img-4", {x: 1000 * 2, duration: 1})
+    .set(".img-2", {opacity: '0', duration: 1}, "<")
+    .to(".img-3", {opacity: '0', duration: 1}, "<")
+    .set(".cover-img-1", {opacity: '0', duration: 1}, "<")
+    .to(".app-container-description", {opacity: '1'})
+
 })
