f34b1508d26b4188916f18fbebfe71565a384ede
libpet-programming <99083200+libpet-programming@users.noreply.github.com>
2024-05-04 10:14:28 +0800

    update index.html

--- js/theme.js
+++ js/theme.js
@@ -1,40 +1,39 @@
-window.onload = function() {
+window.onload = function () {
+  const controller = new ScrollMagic.Controller();
 
-const controller = new ScrollMagic.Controller();
+  /**
+   * Text slide animation (See the Magic)
+   */
+  const fancyText = document.querySelector(".hero-text");
+  const strText = fancyText.textContent;
+  const splitText = strText.split("");
+  fancyText.textContent = "";
+  for (let i = 0; i < splitText.length; i++) {
+    fancyText.innerHTML += "<span>" + splitText[i] + "</span>";
+    //   const span = fancyText.querySelectorAll('span')[i];
+    //   span.classList.add('faded');
+  }
 
-/**
- * Text slide animation (See the Magic)
- */
-const fancyText = document.querySelector(".mission .hero-text");
-const strText = fancyText.textContent;
-const splitText = strText.split("");
-fancyText.textContent = "";
-for (let i = 0; i < splitText.length; i++){
-  fancyText.innerHTML += "<span>" + splitText[i] + "</span>";
-//   const span = fancyText.querySelectorAll('span')[i];
-//   span.classList.add('faded');
-}
-
-const aboutUsBtn = document.getElementById("about-button");
-const staggerTween = TweenMax.staggerFromTo(".faded", 0.01, {opacity:0}, {opacity:1, ease:Back.easeOut}, 0.5);
-let fancyScene = new ScrollMagic.Scene({
-  duration: 500,
-  triggerElement: '.mission',
-  triggerHook: 0.5
-})
-// .addIndicators()
-.on('enter', function(e){
-    for (let i = 0; i < splitText.length; i++){
-        setTimeout(function(){
-            const span = fancyText.querySelectorAll('span')[i];
-            span.classList.add('faded');
-        }, i * 25);
-    }
-    setTimeout(function(){
-      aboutUsBtn.classList.add("appear");
-    }, 2000);
-})
-.addTo(controller);
-
-
+  const staggerTween = TweenMax.staggerFromTo(
+    ".faded",
+    0.01,
+    { opacity: 0 },
+    { opacity: 1, ease: Back.easeOut },
+    0.5
+  );
+  let fancyScene = new ScrollMagic.Scene({
+    duration: 500,
+    triggerElement: ".index-sl",
+    triggerHook: 0.5,
+  })
+    // .addIndicators()
+    .on("enter", function (e) {
+      for (let i = 0; i < splitText.length; i++) {
+        setTimeout(function () {
+          const span = fancyText.querySelectorAll("span")[i];
+          span.classList.add("faded");
+        }, i * 50);
+      }
+    })
+    .addTo(controller);
 };
