e9d968ad3bcec32c31bc3eb3e01b7524ee00d20a
libpet-programming <99083200+libpet-programming@users.noreply.github.com>
2024-06-05 10:53:56 +0800

    update must fill in function

--- js/form.js
+++ js/form.js
@@ -20,20 +20,49 @@ form1.addEventListener("change", (e) => {
 
 form1.addEventListener("click", (e) => {
   if (e.target.id === "submit1") {
+    const currentUrl = window.location.href;
+
     var pageWrapper = document.body;
     var popAlertForm = document.querySelector(".popAlert-form");
     var popAlertFormBtn = document.querySelector(".hover-underline-animation");
     var formOpen = document.querySelector(".formOpen");
 
-    pageWrapper.style.pointerEvents = 'none';
-    pageWrapper.style.userSelect = 'none';
-    popAlertForm.style.pointerEvents = 'none';
-    popAlertForm.style.userSelect = 'none';
-    popAlertFormBtn.style.pointerEvents = 'none';
-    popAlertFormBtn.style.userSelect = 'none';
-    formOpen.style.pointerEvents = 'none';
-    formOpen.style.userSelect = 'none';
-    
+    pageWrapper.style.pointerEvents = "none";
+    pageWrapper.style.userSelect = "none";
+    popAlertForm.style.pointerEvents = "none";
+    popAlertForm.style.userSelect = "none";
+    popAlertFormBtn.style.pointerEvents = "none";
+    popAlertFormBtn.style.userSelect = "none";
+    formOpen.style.pointerEvents = "none";
+    formOpen.style.userSelect = "none";
+
+    // 檢查必填欄位
+    const nameInput = document.getElementById("name");
+    const phoneInput = document.getElementById("phone");
+    const preferredInput = document.getElementById("preferred");
+
+    if (
+      nameInput.value.trim() === "" ||
+      phoneInput.value.trim() === "" ||
+      preferredInput.value.trim() === ""
+    ) {
+      if (currentUrl.includes("/cn/")) {
+        window.alert("請填寫所有必填欄位讓我們處理您的試駕申請! 感謝!");
+      } else {
+        window.alert(
+          "Please fill in all the fields to let us proceed with your test drive booking! Thank you!"
+        );
+      }
+      pageWrapper.style.pointerEvents = "auto";
+      pageWrapper.style.userSelect = "auto";
+      popAlertForm.style.pointerEvents = "auto";
+      popAlertForm.style.userSelect = "auto";
+      popAlertFormBtn.style.pointerEvents = "auto";
+      popAlertFormBtn.style.userSelect = "auto";
+      formOpen.style.pointerEvents = "auto";
+      formOpen.style.userSelect = "auto";
+      return;
+    }
 
     let formdata = new FormData();
     formdata.append("time", new Date());
@@ -62,35 +91,35 @@ form1.addEventListener("click", (e) => {
           );
           // popAlertDescription.classList.remove("show");
 
-          const currentUrl = window.location.href;
-          if (currentUrl.includes('/cn/')) {
-            window.alert("感謝您提供相關資料，我們會盡快聯絡您商量試駕細節。如有任何問題，歡迎隨時透過Whatsapp或電子郵件聯繫我們。");
+          if (currentUrl.includes("/cn/")) {
+            window.alert(
+              "感謝您提供相關資料，我們會盡快聯絡您商量試駕細節。如有任何問題，歡迎隨時透過Whatsapp或電子郵件聯繫我們。"
+            );
           } else {
             window.alert(
               "Thank you for providing your contact information. We will contact you as soon as possible! If you need further information, please feel free to contact us via Whatsapp or Email directly."
             );
           }
-
-
-        } else{
+        } else {
           const currentUrl = window.location.href;
-          if (currentUrl.includes('/cn/')) {
-            window.alert("發送失敗! 請重新嘗試。如問題持續發生，請直接透過Whatsapp或電子郵件聯繫我們。不便之處，敬請原諒！");
+          if (currentUrl.includes("/cn/")) {
+            window.alert(
+              "發送失敗! 請重新嘗試。如問題持續發生，請直接透過Whatsapp或電子郵件聯繫我們。不便之處，敬請原諒！"
+            );
           } else {
             window.alert(
               "Oops! There is some problems when sending your information. Please try again. If this keeps happen, please contact us via Whatsapp or Email directly."
             );
           }
-
         }
-        pageWrapper.style.pointerEvents = 'auto';
-        pageWrapper.style.userSelect = 'auto';
-        popAlertForm.style.pointerEvents = 'auto';
-        popAlertForm.style.userSelect = 'auto';
-        popAlertFormBtn.style.pointerEvents = 'auto';
-        popAlertFormBtn.style.userSelect = 'auto';
-        formOpen.style.pointerEvents = 'auto';
-        formOpen.style.userSelect = 'auto';
+        pageWrapper.style.pointerEvents = "auto";
+        pageWrapper.style.userSelect = "auto";
+        popAlertForm.style.pointerEvents = "auto";
+        popAlertForm.style.userSelect = "auto";
+        popAlertFormBtn.style.pointerEvents = "auto";
+        popAlertFormBtn.style.userSelect = "auto";
+        formOpen.style.pointerEvents = "auto";
+        formOpen.style.userSelect = "auto";
       })
       .catch((error) => console.log("error", error));
   }
@@ -110,9 +139,7 @@ function togglePop(productClick) {
     inputElements[i].value = "";
   }
 
-  var popAlertDescription = document.querySelector(
-    ".popAlert-description"
-  );
+  var popAlertDescription = document.querySelector(".popAlert-description");
   // popAlertDescription.classList.remove("show");
 }
 
@@ -125,9 +152,7 @@ function formOpen() {
   event.preventDefault();
   var popAlertForm = document.querySelector(".popAlert-form");
   popAlertForm.classList.toggle("show");
-  var popAlertDescription = document.querySelector(
-    ".popAlert-description"
-  );
+  var popAlertDescription = document.querySelector(".popAlert-description");
   popAlertDescription.classList.toggle("show");
   var inputElements = document.querySelectorAll("input");
 
